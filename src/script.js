7525c9d1a167291089c74f276c09857cfcc89de9
André Varandas <andre.m.varandas@gmail.com>
2016-06-21 22:31:27 +0100

    Added source files

---
+++ src/script.js
@@ -0,0 +1,19 @@
+var api = 'http://api.icndb.com/jokes/random/';
+(function () {
+    var tweet = $('#tweet');
+    var generateBtn = $('#randomBtn');
+    var targetDiv = $('#quote');
+    $.getJSON(api, function (result) {
+        targetDiv.html(result.value.joke);
+        buildTweet(result.value.joke);
+    });
+    generateBtn.on('click', (function () {
+        $.getJSON(api, function (result) {
+            targetDiv.html(result.value.joke);
+            buildTweet(result.value.joke);
+        });
+    }));
+    var buildTweet = function (quote) {
+        tweet.attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + quote + '"')).attr('target', '_blank');
+    };
+})();
---
+++ src/script.ts
@@ -0,0 +1,30 @@
+const api = 'http://api.icndb.com/jokes/random/';
+
+declare var $;
+
+(():void =>
+{
+    let tweet = $('#tweet');
+    let generateBtn = $('#randomBtn');
+    let targetDiv = $('#quote');
+
+    $.getJSON(api, (result) =>
+    {
+       targetDiv.html(result.value.joke);
+       buildTweet(result.value.joke);
+   });
+
+    generateBtn.on('click', (() =>
+    {
+          $.getJSON(api, (result)=>
+          {
+              targetDiv.html(result.value.joke);
+              buildTweet(result.value.joke);
+          });
+     }));
+
+    let buildTweet = (quote) =>
+    {
+      tweet.attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + quote + '"')).attr('target', '_blank');
+    }
+})();
